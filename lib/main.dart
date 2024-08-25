9fd4a20f96d8b659113e1feb2ae36b8e4a11eb36
faisal <faisalabdalnasser275@gmail.com>
2024-08-25 22:53:01 +0300

    add project name

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Intstagram());
+}
+
+class Intstagram extends StatelessWidget {
+  const Intstagram({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
